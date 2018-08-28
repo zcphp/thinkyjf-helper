c0c45dcfc12c29b3908ca83a59b41adb39a81d50
longlzc <793333136@qq.com>
2018-08-28 13:50:12 +0800

    1

--- src/helper.php
+++ src/helper.php
@@ -1,50 +1,16 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | Thinkyjf
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2018 http://www.thinkyjf.com, All rights reserved.
 // +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: yunwuxin <448901948@qq.com>
+// | Author: Mr.long <admin@zcphp.com>
 // +----------------------------------------------------------------------
 
-//Route::get('captcha/[:id]', "\\think\\captcha\\CaptchaController@index");
-
+//定义助手
 Validate::extend('yjfecho', function ($value, $id = '') {
     return yjfecho($value, $id);
 });
-
-//Validate::setTypeMsg('captcha', ':attribute错误!');
-
-/**
- * @param string $id
- * @param array  $config
- * @return \think\Response
- */
-//function captcha($id = '', $config = [])
-//{
-//    $captcha = new \think\captcha\Captcha($config);
-//    return $captcha->entry($id);
-//}
-
-/**
- * @param $id
- * @return string
- */
-//function captcha_src($id = '')
-//{
-//    return Url::build('/captcha' . ($id ? "/{$id}" : ''));
-//}
-
-/**
- * @param $id
- * @return mixed
- */
-//function captcha_img($id = '')
-//{
-//    return '<img src="' . captcha_src($id) . '" alt="captcha" />';
-//}
 
 /**
  * @param        $value
@@ -52,10 +18,8 @@ Validate::extend('yjfecho', function ($value, $id = '') {
  * @param array  $config
  * @return bool
  */
-function yjfecho($value, $id = '')
+function yjfecho($value)
 {
-	echo 111;
-	return 2;
-    //$captcha = new \think\captcha\Captcha((array) Config::pull('captcha'));
-    //return $captcha->check($value, $id);
+	return $value;
 }
+

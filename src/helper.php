2a2075921ed054b6bbfc44c9248efa5fb5349edc
longlzc <793333136@qq.com>
2018-08-28 14:10:14 +0800

    1

--- src/helper.php
+++ src/helper.php
@@ -6,20 +6,23 @@
 // +----------------------------------------------------------------------
 // | Author: Mr.long <admin@zcphp.com>
 // +----------------------------------------------------------------------
+// | 定义常用的助手
+// +----------------------------------------------------------------------
 
 //定义助手
-Validate::extend('yjfecho', function ($value, $id = '') {
-    return yjfecho($value, $id);
+Validate::extend('yjf_test', function ($value) {
+	//调用本文件中的方法
+    return yjf_test($value);
 });
 
 /**
+ * 测试方法
  * @param        $value
  * @param string $id
  * @param array  $config
  * @return bool
  */
-function yjfecho($value)
-{
+function yjf_test($value){
 	return $value;
 }
 
